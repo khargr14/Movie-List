97fc23660e4ed6bc5f8e98d502bddc21cfb5b86c
khargr14 <kay_hargrove@yahoo.com>
2021-02-19 01:20:41 -0500

    connecting the api

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = Movie.create ([
+    {title: 'Cloudy with a Chance of Meatballs 2', summary: 'Inventor Flint Lockwood battles mutant food beasts created by his notorious machine, including shrimpanzees, tacodiles and jellyfish sandwiches',
+        review_id: 1},
+    { title: 'Spenser Confidential', summary: 'Spenser, an ex-cop and ex-con, teams up with aspiring fighter Hawk to uncover a sinister conspiracy tied to the deaths of two Boston police officers.',
+    review_id: 1}
+
+
+])
+
+
+
+
